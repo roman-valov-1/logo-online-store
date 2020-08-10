9ec61a7e33d5008de469f41a504294efff4b9bc8
roman-valov-1 <62258593+roman-valov-1@users.noreply.github.com>
2020-08-11 00:57:08 +0500

    ...

--- logo-online-store/js/script.js
+++ logo-online-store/js/script.js
@@ -143,6 +143,8 @@ testWebP(function (support) {
     }
 });
 
+
+// Реализация меню бургера через jQuery =====================================
 $(document).ready(function() {
     $('.icon-menu').click(function(event) {
         $('.icon-menu').toggleClass('icon-menu_active');
@@ -155,5 +157,28 @@ $(document).ready(function() {
 $(document).ready(function() {
     $('.menu-page__burger').click(function(event) {
         $('.menu-page__burger').toggleClass('menu-page__burger_active');
+        $('.menu-page__body').slideToggle(500); //выдвигающийся блок 
     })
 })
+
+// Реализация на нативном JS =======================================
+// let menuBurger = document.querySelector('.menu-page__burger');
+
+// menuBurger.addEventListener("click", function(e) {
+//     menuBurger.classList.toggle('_active');
+// });
+
+
+// Реализация всплывающей карточки товара в каталоге (нативный JS)
+let menuParents = document.querySelectorAll('.menu-page__parent');
+
+for (let index = 0; index < menuParents.length; index++) {
+    const menuParent = menuParents[index];
+    menuParent.addEventListener("mouseenter", function(e) {
+        menuParent.classList.add('_active');
+    });
+    menuParent.addEventListener("mouseleave", function(e) {
+        menuParent.classList.remove('_active');
+    });
+}
+
